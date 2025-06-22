0ef6a45b855a3784eb8853b89e303dbcaaada3ee
sebastien <sebastien.savan@gmail.com>
2025-06-22 09:01:58 +0200

    implement hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { navLinks } from "../../constants"
-export default function Navbar() {
+export function Navbar() {
   useGSAP(() => {
     const navTween = gsap.timeline({
       scrollTrigger: {
